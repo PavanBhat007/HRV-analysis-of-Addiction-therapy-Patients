0b39d2fef5d3a079c38a29109a156441fa060f7e
Pavan Bhat <pavanksp612@gmail.com>
2023-12-11 07:53:19 +0530

    added dataset, code and result

--- encrypter.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-fernet = Fernet(key)
-
-def encrypt(data):
-    return fernet.encrypt(data.encode())
-
-def decrypt(data_enc) :   
-    return fernet.decrypt(data_enc).decode()
